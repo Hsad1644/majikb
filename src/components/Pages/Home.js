5339c9a04bfc693187401e1f851c8b74736555b1
Anupam Dash <anupam.dash0505@gmail.com>
2023-01-20 13:58:33 +0530

    router corrections

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,13 @@
+// a basic functional component which has a h1 saying "Home Page"
+// Path: src\components\Pages\Home.js
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Pages/Listings.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Listings = (props) => {
+  return (
+    <div>
+      <h1>Listings Page</h1>
+    </div>
+  );
+};
+
+export default Listings;
