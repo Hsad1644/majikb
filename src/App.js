dbe315bbf96f7c364210345448cd567f55b2f213
Anupam Dash <anupam.dash0505@gmail.com>
2023-01-20 13:36:03 +0530

    React routing correction

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import "./styles.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Header from "./components/Common/Header";
 import Footer from "./components/Common/Footer";
+import Home from "./components/Pages/Home";
+import Listings from "./components/Pages/Listings";
+import Contact from "./components/Pages/Contact";
 
 export default function App() {
   return (
     <div className="App">
       <Router>
-      <Header />
-      <Route path="/" exact component={Home} />
-      <Route path="/listings" component={Listings} />
-      <Route path="/contact" component={Contact} />
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/listings" component={Listings} />
+        <Route path="/contact" component={Contact} />
       </Router>
-
     </div>
   );
 }
