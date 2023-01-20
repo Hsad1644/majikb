fce22f4c83e7b6cfc20f195ea3d7c51fc9286cc4
Anupam Dash <anupam.dash0505@gmail.com>
2023-01-20 13:28:10 +0530

    testing router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./styles.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Header from "./components/Common/Header";
 import Footer from "./components/Common/Footer";
 
 export default function App() {
   return (
     <div className="App">
+      <Router>
       <Header />
+      <Route path="/" exact component={Home} />
+      <Route path="/listings" component={Listings} />
+      <Route path="/contact" component={Contact} />
+      </Router>
+
     </div>
   );
 }
