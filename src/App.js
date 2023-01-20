7986de6931a4077838535c62f019611d8114a5a8
Anupam <72813362+Hsad1644@users.noreply.github.com>
2023-01-20 14:56:44 +0530

    testing routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Common/Header";
 import Footer from "./components/Common/Footer";
 import Home from "./components/Pages/Home";
