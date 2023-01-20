2e544135f2b0dc26942eff7fe5005d62f4ba30d8
Anupam Dash <anupam.dash0505@gmail.com>
2023-01-20 13:17:36 +0530

    testcom header update

---
+++ src/components/Common/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="left-side">
+        <img
+          src={"https://picsum.photos/160?grayscale"}
+          alt="Logo"
+          className="logo"
+        />
+        <h1 className="title">MagicBricks</h1>
+      </div>
+      <div className="right-side">
+        <Link to="/">Home</Link>
+        <Link to="/listings">Listings</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
